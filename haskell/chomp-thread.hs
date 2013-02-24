06cac7199d20a40957cfff12ac1947642d1166bc
inaimathi <inaimathi@lambda>
2013-02-24 01:49:17 -0500

    -Pulled the regex out into a global name

--- haskell/chomp-thread.hs
+++ haskell/chomp-thread.hs
@@ -6,14 +6,15 @@ import Text.Regex.Posix (getAllTextMatches, (=~))
 import System.Process (system)
 import System.Directory (doesFileExist)
 
+picReg = "//images.4chan.org/[a-z]+/src/([0-9]+).(jpg|png|gif)"
+
 main = do
   res <- simpleHTTP (getRequest "http://boards.4chan.org/gif/res/5493461") >>= getResponseBody
   mapM_ getNewPic $ picURIs res
 
 picURIs :: String -> [String]
 picURIs html = everySecond matches
-  where matches = getAllTextMatches $ html =~ reg
-        reg = "//images.4chan.org/[a-z]+/src/([0-9]+).(jpg|png|gif)"
+  where matches = getAllTextMatches $ html =~ picReg
 
 getNewPic uri = do
   exists <- doesFileExist fname
