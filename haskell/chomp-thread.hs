d51570a897fefac992efb7db53ecfa9b75333d8c
inaimathi <inaimathi@lambda>
2013-02-24 01:48:27 -0500

    -first stab at 4chan thread downloader

---
+++ haskell/chomp-thread.hs
@@ -0,0 +1,30 @@
+module Main where
+
+import Network.HTTP
+import Data.Text (breakOnEnd, pack, unpack)
+import Text.Regex.Posix (getAllTextMatches, (=~))
+import System.Process (system)
+import System.Directory (doesFileExist)
+
+main = do
+  res <- simpleHTTP (getRequest "http://boards.4chan.org/gif/res/5493461") >>= getResponseBody
+  mapM_ getNewPic $ picURIs res
+
+picURIs :: String -> [String]
+picURIs html = everySecond matches
+  where matches = getAllTextMatches $ html =~ reg
+        reg = "//images.4chan.org/[a-z]+/src/([0-9]+).(jpg|png|gif)"
+
+getNewPic uri = do
+  exists <- doesFileExist fname
+  case exists of
+    False -> do getPic uri
+                return $ Just uri
+    True -> return $ Nothing
+  where fname = unpack . snd $ breakOnEnd (pack "/") $ pack uri
+
+getPic uri = system $ "wget http:" ++ uri
+
+everySecond :: [a] -> [a]
+everySecond (a:b:rest) = a:as where as = everySecond rest
+everySecond _ = []
